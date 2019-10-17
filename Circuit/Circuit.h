8073cf69a1d6a331620b2e422e64742bae224d57
Tianyu Xu <53030335+CPT-1@users.noreply.github.com>
2019-10-17 12:27:12 +0800

    Update Circuit.h
    Add the function of calculationg AC

--- Circuit/Circuit.h
+++ Circuit/Circuit.h
@@ -1,20 +1,20 @@
-//A program to solve linear circuitry
-
 #pragma once
 
 #include <iostream>
 #include <cstdio>
+#include <ccomplex>
 
 #define Maxsize 101
-#define Given 1
+#define Given 1 
 
+typedef std::complex<double> DataType;
 typedef struct {
-	double U;
+	DataType U;
 	int isConst;
 	//1 means Given 
 	//-1 means it is a vertex of constant current source 
 	//0 means unkonwn
-	double *Factor;
+	DataType*Factor;
 }Set_U;
 
 //Definition of Edge
@@ -22,9 +22,12 @@ typedef struct _enode {
 	int num;
 	int Ver_From;
 	int Ver_To;
-	int isConst;//1 means Given 0 unknown
-	double I;
-	double R;
+	int isConst;	//1 means Given 0 unknown
+	DataType I;
+	//if R=-Xcj means the load is a capacitor
+	//if R=XLj means the load is a coil
+	//if R is a real num means the load is a resistor
+	DataType R;		 
 	struct _enode *next;
 }ENode;
 typedef ENode *PtrToEdge;
@@ -33,8 +36,8 @@ typedef ENode *PtrToEdge;
 //Definition of Adjoin Vertex List
 typedef struct _adjvNode {
 	int Ver;
-	double U;
-	PtrToEdge firstedge;//in most case the firstedge keep empty
+	DataType U;
+	PtrToEdge firstedge;	//in most case the firstedge keep empty
 }AdjVList;
 typedef AdjVList *PtrToAdjV;
 
@@ -43,11 +46,12 @@ typedef AdjVList *PtrToAdjV;
 class Graph
 {
 private:
-	int Nv;//number of vertex
-	int Ne;//number of edge
-	int Unkonwn;//number of unknown
-	double *B;
-	Set_U *SU;//to store the Factor
+	int Nv;		//number of vertex
+	int Ne;		//number of edge
+	int Unkonwn;	//number of unknown
+	double Frequence;	//the f of I 
+	DataType *B;
+	Set_U *SU;		//to store the Factor
 	PtrToAdjV adjv;
 public:
 	void InitG(); 
@@ -64,16 +68,16 @@ typedef Graph* PtrToGraph;
 class Determinant
 {
 private:
-	double *Det;
-	double det;
+	DataType*Det;
+	DataType det;
 	int n;
 public:
 	void InitDet(Set_U *SU, int N);
 	int GetN();
-	double *GetD();
-	double DetCalculate(double *Det, int n);
-	double *Division(double *Det, int n, int i);
-	double CalculateLinear(double *B, int i);
+	DataType*GetD();
+	DataType DetCalculate(DataType*Det, int n);
+	DataType*Division(DataType*Det, int n, int i);
+	DataType CalculateLinear(DataType*B, int i);
 };
 
 
